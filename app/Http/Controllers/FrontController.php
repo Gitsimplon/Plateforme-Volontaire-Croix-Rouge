463068b46f771554e369884ecfcbbd431856f8c4
Abdoulaye Maiga A.K <andhackroid@gmail.com>
2025-10-22 15:13:36 +0100

    les routes principales

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,5 +6,53 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('volontaire.index');
+    }
+
+    public function calendar()
+    {
+        return view('volontaire.calendar');
+    }
+
+    public function dashboard()
+    {
+        return view('volontaire.dashboard');
+    }
+
+    public function documents()
+    {
+        return view('volontaire.documents');
+    }
+
+    public function login()
+    {
+        return view('volontaire.login');
+    }
+
+    public function logout()
+    {
+        return view('volontaire.logout');
+    }
+
+    public function messages()
+    {
+        return view('volontaire.messages');
+    }
+
+    public function missions()
+    {
+        return view('volontaire.missions');
+    }
+
+    public function profile()
+    {
+        return view('volontaire.profile');
+    }
+
+    public function register()
+    {
+        return view('volontaire.register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('volontaire.index');
 });
+
+// Autres pages
+Route::get('/', [FrontController::class, 'index'])->name('home');
+Route::get('/calendar', [FrontController::class, 'calendar'])->name('calendar');
+Route::get('/dashboard', [FrontController::class, 'dashboard'])->name('dashboard');
+Route::get('/documents', [FrontController::class, 'documents'])->name('documents');
+Route::get('/login', [FrontController::class, 'login'])->name('login');
+Route::get('/logout', [FrontController::class, 'logout'])->name('logout');
+Route::get('/messages', [FrontController::class, 'messages'])->name('messages');
+Route::get('/missions', [FrontController::class, 'missions'])->name('missions');
+Route::get('/profile', [FrontController::class, 'profile'])->name('profile');
+Route::get('/register', [FrontController::class, 'register'])->name('register');
